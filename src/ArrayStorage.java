76bb121272a025615072d0bdb72e689c53116526
Dmitry Luzko <dmitrii.luzko@gmail.com>
2020-04-03 13:51:44 +0300

    first homework completed by Luzko Dmitry

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,66 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        int sizeStorage = size();
+        for (int i = 0; i < sizeStorage; i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        int sizeStorage = size();
+        if (sizeStorage >= storage.length) {
+            Resume[] storageTemp = new Resume[storage.length];
+            System.arraycopy(storage, 0, storageTemp, 0, storage.length);
+            storage = new Resume[(int) (storageTemp.length * 1.5 + 1)];
+            System.arraycopy(storageTemp, 0, storage, 0, storageTemp.length);
+        }
+        storage[sizeStorage] = r;
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume resume = null;
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                resume = storage[i];
+                break;
+            }
+        }
+        return resume;
     }
 
     void delete(String uuid) {
+        int sizeStorage = size();
+        int index = -1;
+        for (int i = 0; i < sizeStorage; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                index = i;
+                storage[i] = null;
+                break;
+            }
+        }
+        if (index != -1) {
+            System.arraycopy(storage, index + 1, storage, index, sizeStorage - index);
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] resumes = new Resume[size()];
+        System.arraycopy(storage, 0, resumes, 0, size());
+        return resumes;
     }
 
     int size() {
-        return 0;
+        int count = 0;
+        for (Resume resume : storage) {
+            if (resume == null) {
+                break;
+            }
+            count++;
+        }
+        return count;
     }
 }
