2ca17d1a360c3ffad3d8939c7c000021ff09da8c
Dmitry Luzko <dmitrii.luzko@gmail.com>
2020-04-14 17:30:11 +0300

    fixed test classes

--- test/ru/javawebinar/basejava/storage/ArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/ArrayStorageTest.java
@@ -1,25 +1,8 @@
 package ru.javawebinar.basejava.storage;
 
-import org.junit.Test;
-
 public class ArrayStorageTest extends AbstractArrayStorageTest{
 
     public ArrayStorageTest() {
         super(new ArrayStorage());
-    }
-
-    @Test
-    public void fillDeletedElement() {
-
-    }
-
-    @Test
-    public void insertElement() {
-
-    }
-
-    @Test
-    public void getIndex() {
-
     }
 }
--- test/ru/javawebinar/basejava/storage/SortedArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/SortedArrayStorageTest.java
@@ -1,25 +1,8 @@
 package ru.javawebinar.basejava.storage;
 
-import org.junit.Test;
-
 public class SortedArrayStorageTest extends AbstractArrayStorageTest{
 
     public SortedArrayStorageTest() {
         super(new SortedArrayStorage());
-    }
-
-    @Test
-    public void fillDeletedElement() {
-
-    }
-
-    @Test
-    public void insertElement() {
-
-    }
-
-    @Test
-    public void getIndex() {
-
     }
 }
